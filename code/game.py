47c0b84fdf2ee7f7d10b626d55dbd91d1d6abeee
Max Rodrigues <max@email.com>
2025-02-28 11:15:44 -0300

    Adding classes generated from StarUML

---
+++ code/game.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Game:
+    def __init__(self):
+        self.window = None
+        self.Attribute4 = None
+        self.Attribute1 = None
+        self.Attribute2 = None
+        self.Attribute3 = None
+
+    def run(self, ):
+        pass
